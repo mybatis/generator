dfeb2512138ef144328712f4fe74975c53763883
Jeff Butler <jeffgbutler@gmail.com>
2010-03-18 19:56:23 +0000

    [ibator] found another character to ignore when creating property names

--- ibator-core/src/main/java/org/apache/ibatis/ibator/internal/util/JavaBeansUtil.java
+++ ibator-core/src/main/java/org/apache/ibatis/ibator/internal/util/JavaBeansUtil.java
@@ -113,6 +113,7 @@ public class JavaBeansUtil {
             case '#':
             case ' ':
             case '/':
+            case '&':
 				if (sb.length() > 0) {
 					nextUpperCase = true;
 				}

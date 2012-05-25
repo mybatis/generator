a700d729d8b3e853c249a037970a5e88874aae77
Jeff Butler <jeffgbutler@gmail.com>
2012-05-25 18:01:11 +0000

    Add virtual primary key plugin

---
+++ core/mybatis-generator-core/src/main/java/org/mybatis/generator/plugins/VirtualPrimaryKeyPlugin.java
@@ -0,0 +1,58 @@
+/*
+ *  Copyright 2012 The MyBatis Team
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+package org.mybatis.generator.plugins;
+
+import java.util.List;
+import java.util.StringTokenizer;
+
+import org.mybatis.generator.api.IntrospectedTable;
+import org.mybatis.generator.api.PluginAdapter;
+
+/**
+ * This plugin can be used to specify columns that act as a primary key, even
+ * if they are not strictly defined as primary keys in the database.
+ * 
+ * To use the plugin, add a property to the table configuration
+ * specifying a comma delimited list of column names to use as a primary
+ * key:
+ * 
+ * <table...>
+ *   <property name="virtualKeyColumns" value="ID1,ID2"/>
+ * </table>
+ * 
+ * @author Jeff Butler
+ *
+ */
+public class VirtualPrimaryKeyPlugin extends PluginAdapter {
+
+    public boolean validate(List<String> warnings) {
+        return true;
+    }
+
+    @Override
+    public void initialized(IntrospectedTable introspectedTable) {
+        String virtualKey =
+        introspectedTable.getTableConfiguration().getProperty("virtualKeyColumns");
+        
+        if (virtualKey != null) {
+            StringTokenizer st = new StringTokenizer(virtualKey, ", ", false);
+            while (st.hasMoreTokens()) {
+                String column = st.nextToken();
+                introspectedTable.addPrimaryKeyColumn(column);
+            }
+        }
+    }
+}

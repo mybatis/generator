afda15230a196e9866ff4d1f5455321ff152ba41
kozake <kozake@dcr.co.jp>
2016-10-18 19:54:20 +0900

    Support Inner Interface.

---
+++ core/mybatis-generator-core/src/test/java/org/mybatis/generator/api/dom/java/InnerInterfaceTest.java
@@ -0,0 +1,99 @@
+/**
+ *    Copyright 2006-2016 the original author or authors.
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
+package org.mybatis.generator.api.dom.java;
+
+import org.junit.Test;
+
+import static junit.framework.TestCase.assertFalse;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertSame;
+import static org.junit.Assert.assertTrue;
+
+public class InnerInterfaceTest {
+
+    @Test
+    public void testConstructor() {
+
+        InnerInterface innerInterface = new InnerInterface("com.foo.InnerUserInterface");
+        assertNotNull(innerInterface);
+    }
+
+    @Test
+    public void testAddSuperInterface() {
+
+        InnerInterface innerInterface = new InnerInterface("com.foo.InnerUserInterface");
+        FullyQualifiedJavaType superInterType = new FullyQualifiedJavaType("com.foo.SuperUserInterface");
+        innerInterface.addSuperInterface(superInterType);
+        assertNotNull(innerInterface.getSuperInterfaceTypes());
+        assertTrue(innerInterface.getSuperInterfaceTypes().contains(superInterType));
+    }
+
+    @Test
+    public void testAddMethod() {
+
+        InnerInterface interfaze = new InnerInterface("com.foo.UserInterface");
+        Method method = new Method();
+        interfaze.addMethod(method);
+
+        assertNotNull(interfaze.getMethods());
+        assertEquals(interfaze.getMethods().size(), 1);
+        assertSame(interfaze.getMethods().get(0), method);
+    }
+
+    @Test
+    public void testGetType() {
+
+        InnerInterface innerInterface = new InnerInterface("com.foo.InnerUserInterface");
+        assertNotNull(innerInterface.getType());
+        assertEquals(innerInterface.getType().getFullyQualifiedName(), "com.foo.InnerUserInterface");
+    }
+
+    @Test
+    public void testGetSuperClass() {
+
+        InnerInterface interfaze = new InnerInterface("com.foo.UserInterface");
+        assertNull(interfaze.getSuperClass());
+    }
+
+
+    @Test
+    public void testAddInnerInterfaces() {
+
+        InnerInterface interfaze = new InnerInterface("com.foo.UserInterface");
+        InnerInterface innerInterfaze = new InnerInterface("com.foo.InnerUserInterface");
+
+        interfaze.addInnerInterfaces(innerInterfaze);
+        assertNotNull(interfaze.getInnerInterfaces());
+        assertEquals(interfaze.getInnerInterfaces().size(), 1);
+        assertSame(interfaze.getInnerInterfaces().get(0), innerInterfaze);
+    }
+
+    @Test
+    public void testIsJavaInterface() {
+
+        InnerInterface interfaze = new InnerInterface("com.foo.UserInterface");
+        assertTrue(interfaze.isJavaInterface());
+    }
+
+    @Test
+    public void testIsJavaEnumeration() {
+
+        InnerInterface interfaze = new InnerInterface("com.foo.UserInterface");
+        assertFalse(interfaze.isJavaEnumeration());
+    }
+}
---
+++ core/mybatis-generator-core/src/test/java/org/mybatis/generator/api/dom/java/InterfaceTest.java
@@ -0,0 +1,103 @@
+/**
+ *    Copyright 2006-2016 the original author or authors.
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
+package org.mybatis.generator.api.dom.java;
+
+import org.junit.Test;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+
+public class InterfaceTest {
+
+    @Test
+    public void testConstructor() {
+
+        Interface interfaze = new Interface("com.foo.UserInterface");
+        assertNotNull(interfaze);
+    }
+
+    @Test
+    public void testAddImportedType() {
+
+        Interface interfaze = new Interface("com.foo.UserInterface");
+        FullyQualifiedJavaType arrayList = FullyQualifiedJavaType.getNewArrayListInstance();
+        interfaze.addImportedType(arrayList);
+
+        assertNotNull(interfaze.getImportedTypes());
+        assertEquals(interfaze.getImportedTypes().size(), 1);
+        assertTrue(interfaze.getImportedTypes().contains(arrayList));
+    }
+
+    @Test
+    public void testAddImportedTypes() {
+
+        Interface interfaze = new Interface("com.foo.UserInterface");
+        Set<FullyQualifiedJavaType> importedTypes = new HashSet<FullyQualifiedJavaType>();
+
+        FullyQualifiedJavaType arrayList = FullyQualifiedJavaType.getNewArrayListInstance();
+        FullyQualifiedJavaType hashMap = FullyQualifiedJavaType.getNewHashMapInstance();
+
+        importedTypes.add(arrayList);
+        importedTypes.add(hashMap);
+        interfaze.addImportedTypes(importedTypes);
+
+        assertNotNull(interfaze.getImportedTypes());
+        assertEquals(interfaze.getImportedTypes().size(), 2);
+        assertTrue(interfaze.getImportedTypes().contains(arrayList));
+        assertTrue(interfaze.getImportedTypes().contains(hashMap));
+    }
+
+    @Test
+    public void testAddFileCommentLine() {
+
+        Interface interfaze = new Interface("com.foo.UserInterface");
+        interfaze.addFileCommentLine("test");
+
+        assertNotNull(interfaze.getFileCommentLines());
+        assertEquals(interfaze.getFileCommentLines().size(), 1);
+        assertEquals(interfaze.getFileCommentLines().get(0), "test");
+    }
+
+    @Test
+    public void testAddStaticImport() {
+
+        Interface interfaze = new Interface("com.foo.UserInterface");
+        interfaze.addStaticImport("com.foo.StaticUtil");
+
+        assertNotNull(interfaze.getStaticImports());
+        assertEquals(interfaze.getStaticImports().size(), 1);
+        assertTrue(interfaze.getStaticImports().contains("com.foo.StaticUtil"));
+    }
+
+    @Test
+    public void testAddStaticImports() {
+
+        Interface interfaze = new Interface("com.foo.UserInterface");
+        Set<String> staticImports = new HashSet<String>();
+        staticImports.add("com.foo.StaticUtil1");
+        staticImports.add("com.foo.StaticUtil2");
+        interfaze.addStaticImports(staticImports);
+
+        assertNotNull(interfaze.getStaticImports());
+        assertEquals(interfaze.getStaticImports().size(), 2);
+        assertTrue(interfaze.getStaticImports().contains("com.foo.StaticUtil1"));
+        assertTrue(interfaze.getStaticImports().contains("com.foo.StaticUtil2"));
+    }
+}

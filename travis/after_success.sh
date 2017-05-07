f5489cbf8da8d4a0551cef02d6d79ff837894167
Jeremy Landis <jeremylandis@hotmail.com>
2017-05-07 19:39:16 -0400

    [ci] Adjustments for travis build

--- travis/after_success.sh
+++ travis/after_success.sh
@@ -1,20 +1,8 @@
 #!/bin/bash
 
-# Get Project Repo
-mybatis_repo=$(git config --get remote.origin.url 2>&1)
-echo "Repo detected: ${mybatis_repo}"
-
 # Get Commit Message
 commit_message=$(git log --format=%B -n 1)
 echo "Current commit detected: ${commit_message}"
-
-# Get the Java version.
-# Java 1.5 will give 15.
-# Java 1.6 will give 16.
-# Java 1.7 will give 17.
-# Java 1.8 will give 18.
-VER=`java -version 2>&1 | sed 's/java version "\(.*\)\.\(.*\)\..*"/\1\2/; 1q'`
-echo "Java detected: ${VER}"
 
 # We build for several JDKs on Travis.
 # Some actions, like analyzing the code (Coveralls) and uploading
@@ -29,21 +17,27 @@ echo "Java detected: ${VER}"
 # 3. Deploy site (disabled)
 #    a. Use -q option to only display Maven errors and warnings.
 
-if [ "$mybatis_repo" == "https://github.com/mybatis/generator.git" ] && [ "$TRAVIS_PULL_REQUEST" == "false" ] && [ "$TRAVIS_BRANCH" == "master" ] && [[ "$commit_message" != *"[maven-release-plugin]"* ]]; then
-  if [ $VER == "18" ]; then
+if [ $TRAVIS_REPO_SLUG == "mybatis/generator" ] && [ "$TRAVIS_PULL_REQUEST" == "false" ] && [ "$TRAVIS_BRANCH" == "master" ] && [[ "$commit_message" != *"[maven-release-plugin]"* ]]; then
+
+  if [ $TRAVIS_JDK_VERSION == "oraclejdk8" ]; then
+
     # Deploy to Sonatype
-    mvn clean deploy -q --settings ../travis/settings.xml
+    ./mvnw clean deploy -q --settings ../travis/settings.xml
     echo -e "Successfully deployed SNAPSHOT artifacts to Sonatype under Travis job ${TRAVIS_JOB_NUMBER}"
 
 	# Deploy to Coveralls
-    mvn clean test jacoco:report-integration coveralls:report -q
+    ./mvnw clean test jacoco:report-integration coveralls:report -q ../travis/settings.xml
     echo -e "Successfully ran coveralls under Travis job ${TRAVIS_JOB_NUMBER}"
 
 	# Deploy to site
 	# various issues exist currently in building this so comment for now
-	# mvn site site:deploy -q
+	# ./mvnw site site:deploy -q ../travis/settings.xml
 	# echo -e "Successfully deploy site under Travis job ${TRAVIS_JOB_NUMBER}"
+  else
+    echo "Java Version does not support additonal activity for travis CI"
   fi
 else
+  echo "Travis Pull Request: $TRAVIS_PULL_REQUEST"
+  echo "Travis Branch: $TRAVIS_BRANCH"
   echo "Travis build skipped"
 fi

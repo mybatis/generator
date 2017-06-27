5f69fd997852065b1eb2509f6f3071c9d9959ee6
dkzhang <zdk@dlyt.net>
2017-06-27 16:39:56 +0800

    overwrite

--- core/mybatis-generator-core-ext/src/main/java/org/mybatis/generator/api/MyBatisGenerator.java
+++ core/mybatis-generator-core-ext/src/main/java/org/mybatis/generator/api/MyBatisGenerator.java
@@ -340,13 +340,13 @@ public class MyBatisGenerator {
                     .getTargetProject(), gxf.getTargetPackage());
             targetFile = new File(directory, gxf.getFileName());
             if (targetFile.exists()) {
-                if (gxf.isMergeable()) {
-                    source = XmlFileMergerJaxp.getMergedSource(gxf,
-                            targetFile);
-                } else if (shellCallback.isOverwriteEnabled()) {
+                if (shellCallback.isOverwriteEnabled()) {
                     source = gxf.getFormattedContent();
                     warnings.add(getString("Warning.11", //$NON-NLS-1$
                             targetFile.getAbsolutePath()));
+                } else if (gxf.isMergeable()) {
+                    source = XmlFileMergerJaxp.getMergedSource(gxf,
+                            targetFile);
                 } else {
                     source = gxf.getFormattedContent();
                     targetFile = getUniqueFileName(directory, gxf

c6466816baf96e340bd72bfeac34d323a6a31b57
Jeff Butler <jeffgbutler@gmail.com>
2018-01-07 16:58:33 -0500

    Add UnmergeableXmlMappersPlugin

---
+++ core/mybatis-generator-core/src/main/java/org/mybatis/generator/plugins/UnmergeableXmlMappersPlugin.java
@@ -0,0 +1,46 @@
+/**
+ *    Copyright 2006-2018 the original author or authors.
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
+package org.mybatis.generator.plugins;
+
+import java.util.List;
+
+import org.mybatis.generator.api.GeneratedXmlFile;
+import org.mybatis.generator.api.IntrospectedTable;
+import org.mybatis.generator.api.PluginAdapter;
+
+/**
+ * This plugin marks generated XML mapper files as unmergeable.  This will cause the generator to either
+ * overwrite the files, or save the files under a new name depending on how the overwrite setting is configured.
+ *   
+ * This can be useful when comments are disabled so the normal XML merge won't work. 
+ * 
+ * @author Jeff Butler
+ * 
+ */
+public class UnmergeableXmlMappersPlugin extends PluginAdapter {
+
+    @Override
+    public boolean validate(List<String> warnings) {
+        return true;
+    }
+
+    @Override
+    public boolean sqlMapGenerated(GeneratedXmlFile sqlMap,
+            IntrospectedTable introspectedTable) {
+        sqlMap.setMergeable(false);
+        return true;
+    }
+}

3935e3bb6b53c7766babbdbca92d0783c7228ff0
Jeff Butler <jeffgbutler@gmail.com>
2018-07-05 17:58:52 -0400

    Add context property to specify Java8+ as a target

--- core/mybatis-generator-core/src/main/java/org/mybatis/generator/config/PropertyRegistry.java
+++ core/mybatis-generator-core/src/main/java/org/mybatis/generator/config/PropertyRegistry.java
@@ -54,6 +54,7 @@ public class PropertyRegistry {
     public static final String CONTEXT_JAVA_FILE_ENCODING = "javaFileEncoding"; //$NON-NLS-1$
     public static final String CONTEXT_JAVA_FORMATTER = "javaFormatter"; //$NON-NLS-1$
     public static final String CONTEXT_XML_FORMATTER = "xmlFormatter"; //$NON-NLS-1$
+    public static final String CONTEXT_TARGET_JAVA8 = "targetJava8"; //$NON-NLS-1$
 
     public static final String CLIENT_USE_LEGACY_BUILDER = "useLegacyBuilder"; //$NON-NLS-1$
     

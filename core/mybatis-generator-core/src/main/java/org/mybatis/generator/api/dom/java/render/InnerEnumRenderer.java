0142e7594f9ae1752aa418b7c2e918265a4c3632
Jeff Butler <jeffgbutler@gmail.com>
2019-07-09 07:29:37 -0400

    Copy/Paste Reduction

--- core/mybatis-generator-core/src/main/java/org/mybatis/generator/api/dom/java/render/InnerEnumRenderer.java
+++ core/mybatis-generator-core/src/main/java/org/mybatis/generator/api/dom/java/render/InnerEnumRenderer.java
@@ -1,5 +1,5 @@
 /**
- *    Copyright 2006-2018 the original author or authors.
+ *    Copyright 2006-2019 the original author or authors.
  *
  *    Licensed under the Apache License, Version 2.0 (the "License");
  *    you may not use this file except in compliance with the License.
@@ -48,10 +48,7 @@ public class InnerEnumRenderer {
         lines.addAll(renderInnerInterfaces(innerEnum.getInnerInterfaces(), compilationUnit));
         lines.addAll(renderInnerEnums(innerEnum.getInnerEnums(), compilationUnit));
 
-        // last line might be blank, remove it if so
-        if (lines.get(lines.size() - 1).isEmpty()) {
-            lines = lines.subList(0, lines.size() - 1);
-        }
+        lines = RenderingUtilities.removeLastEmptyLine(lines);
 
         lines.add("}"); //$NON-NLS-1$
 
--- core/mybatis-generator-core/src/main/java/org/mybatis/generator/api/dom/java/render/RenderingUtilities.java
+++ core/mybatis-generator-core/src/main/java/org/mybatis/generator/api/dom/java/render/RenderingUtilities.java
@@ -1,5 +1,5 @@
 /**
- *    Copyright 2006-2018 the original author or authors.
+ *    Copyright 2006-2019 the original author or authors.
  *
  *    Licensed under the Apache License, Version 2.0 (the "License");
  *    you may not use this file except in compliance with the License.
@@ -193,5 +193,13 @@ public class RenderingUtilities {
         }
         
         return JAVA_INDENT + in;
+    }
+    
+    public static List<String> removeLastEmptyLine(List<String> lines) {
+        if (lines.get(lines.size() - 1).isEmpty()) {
+            return lines.subList(0, lines.size() - 1);
+        } else {
+            return lines;
+        }
     }
 }

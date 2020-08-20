db838b7891927434f180348d72addfdb504cfa35
Jeff Butler <jeffgbutler@gmail.com>
2020-08-20 10:52:46 -0400

    Serializable plugin should work for Kotlin Data Classes

--- core/mybatis-generator-core/src/main/java/org/mybatis/generator/api/dom/kotlin/KotlinType.java
+++ core/mybatis-generator-core/src/main/java/org/mybatis/generator/api/dom/kotlin/KotlinType.java
@@ -1,5 +1,5 @@
 /**
- *    Copyright 2006-2019 the original author or authors.
+ *    Copyright 2006-2020 the original author or authors.
  *
  *    Licensed under the Apache License, Version 2.0 (the "License");
  *    you may not use this file except in compliance with the License.
@@ -62,6 +62,10 @@ public class KotlinType extends KotlinNamedItemContainer {
     
     public void addConstructorProperty(KotlinProperty property) {
         constructorProperties.add(property);
+    }
+
+    public void addSuperType(String superType) {
+        superTypes.add(superType);
     }
 
     @Override

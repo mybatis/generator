600c0a784729b870feac82700903eb46b2feccf4
wangjie <wangjie_fourth@163.com>
2020-09-02 15:03:50 +0800

    refine some code with java 8

--- core/mybatis-generator-core/src/main/java/org/mybatis/generator/internal/db/DatabaseIntrospector.java
+++ core/mybatis-generator-core/src/main/java/org/mybatis/generator/internal/db/DatabaseIntrospector.java
@@ -1,5 +1,5 @@
 /**
- *    Copyright 2006-2019 the original author or authors.
+ *    Copyright 2006-2020 the original author or authors.
  *
  *    Licensed under the Apache License, Version 2.0 (the "License");
  *    you may not use this file except in compliance with the License.
@@ -547,11 +547,7 @@ public class DatabaseIntrospector {
                     rs.getString("TABLE_SCHEM"), //$NON-NLS-1$
                     rs.getString("TABLE_NAME")); //$NON-NLS-1$
 
-            List<IntrospectedColumn> columns = answer.get(atn);
-            if (columns == null) {
-                columns = new ArrayList<>();
-                answer.put(atn, columns);
-            }
+            List<IntrospectedColumn> columns = answer.computeIfAbsent(atn, k -> new ArrayList<>());
 
             columns.add(introspectedColumn);
 

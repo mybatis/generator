343d8896496422f85f45dcbbd097d02b26ef7fd3
Jeff Butler <jeffgbutler@gmail.com>
2021-05-13 11:34:43 -0400

    Remove deprecated methods

--- core/mybatis-generator-core/src/main/java/org/mybatis/generator/plugins/dsql/DisableDeletePlugin.java
+++ core/mybatis-generator-core/src/main/java/org/mybatis/generator/plugins/dsql/DisableDeletePlugin.java
@@ -1,5 +1,5 @@
 /*
- *    Copyright 2006-2020 the original author or authors.
+ *    Copyright 2006-2021 the original author or authors.
  *
  *    Licensed under the Apache License, Version 2.0 (the "License");
  *    you may not use this file except in compliance with the License.
@@ -25,7 +25,7 @@ import org.mybatis.generator.api.dom.kotlin.KotlinFile;
 import org.mybatis.generator.api.dom.kotlin.KotlinFunction;
 
 /**
- * Disables all delete methods in the MyBatisDynamicSQLV2 runtime.
+ * Disables all delete methods in the MyBatis3DynamicSQL and MyBatis3Kotlin runtimes.
  *
  * @author Jeff Butler
  *

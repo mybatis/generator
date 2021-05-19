f0bf101358534ef4b9223d05ee647e778fbdb883
Jeff Butler <jeffgbutler@gmail.com>
2021-05-19 18:28:37 -0400

    Common Primary Key Method Parameters

--- core/mybatis-generator-core/src/main/java/org/mybatis/generator/codegen/mybatis3/javamapper/elements/AbstractJavaMapperMethodGenerator.java
+++ core/mybatis-generator-core/src/main/java/org/mybatis/generator/codegen/mybatis3/javamapper/elements/AbstractJavaMapperMethodGenerator.java
@@ -25,17 +25,18 @@ import static org.mybatis.generator.internal.util.StringUtility.stringHasValue;
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
+import java.util.Set;
 
 import org.mybatis.generator.api.IntrospectedColumn;
 import org.mybatis.generator.api.dom.java.FullyQualifiedJavaType;
 import org.mybatis.generator.api.dom.java.Interface;
 import org.mybatis.generator.api.dom.java.Method;
+import org.mybatis.generator.api.dom.java.Parameter;
 import org.mybatis.generator.codegen.AbstractGenerator;
 import org.mybatis.generator.codegen.mybatis3.ListUtilities;
 import org.mybatis.generator.config.GeneratedKey;
 
-public abstract class AbstractJavaMapperMethodGenerator extends
-        AbstractGenerator {
+public abstract class AbstractJavaMapperMethodGenerator extends AbstractGenerator {
     public abstract void addInterfaceElements(Interface interfaze);
 
     protected AbstractJavaMapperMethodGenerator() {
@@ -86,7 +87,6 @@ public abstract class AbstractJavaMapperMethodGenerator extends
                 sb.append("@Options(useGeneratedKeys=true,keyProperty=\""); //$NON-NLS-1$
                 sb.append(introspectedColumn.getJavaProperty());
                 sb.append("\")"); //$NON-NLS-1$
-                method.addAnnotation(sb.toString());
             } else {
                 FullyQualifiedJavaType fqjt = introspectedColumn.getFullyQualifiedJavaType();
                 sb.append("@SelectKey(statement=\""); //$NON-NLS-1$
@@ -98,8 +98,8 @@ public abstract class AbstractJavaMapperMethodGenerator extends
                 sb.append(", resultType="); //$NON-NLS-1$
                 sb.append(fqjt.getShortName());
                 sb.append(".class)"); //$NON-NLS-1$
-                method.addAnnotation(sb.toString());
             }
+            method.addAnnotation(sb.toString());
         });
     }
 
@@ -204,5 +204,38 @@ public abstract class AbstractJavaMapperMethodGenerator extends
 
         answer.add("})"); //$NON-NLS-1$
         return answer;
+    }
+
+    protected void addPrimaryKeyMethodParameters(boolean isSimple, Method method,
+                                                 Set<FullyQualifiedJavaType> importedTypes) {
+        if (!isSimple && introspectedTable.getRules().generatePrimaryKeyClass()) {
+            FullyQualifiedJavaType type = new FullyQualifiedJavaType(introspectedTable.getPrimaryKeyType());
+            importedTypes.add(type);
+            method.addParameter(new Parameter(type, "key")); //$NON-NLS-1$
+        } else {
+            // no primary key class - fields are in the base class
+            // if more than one PK field, then we need to annotate the
+            // parameters for MyBatis3
+            List<IntrospectedColumn> introspectedColumns = introspectedTable
+                    .getPrimaryKeyColumns();
+            boolean annotate = introspectedColumns.size() > 1;
+            if (annotate) {
+                importedTypes.add(new FullyQualifiedJavaType("org.apache.ibatis.annotations.Param")); //$NON-NLS-1$
+            }
+            StringBuilder sb = new StringBuilder();
+            for (IntrospectedColumn introspectedColumn : introspectedColumns) {
+                FullyQualifiedJavaType type = introspectedColumn.getFullyQualifiedJavaType();
+                importedTypes.add(type);
+                Parameter parameter = new Parameter(type, introspectedColumn.getJavaProperty());
+                if (annotate) {
+                    sb.setLength(0);
+                    sb.append("@Param(\""); //$NON-NLS-1$
+                    sb.append(introspectedColumn.getJavaProperty());
+                    sb.append("\")"); //$NON-NLS-1$
+                    parameter.addAnnotation(sb.toString());
+                }
+                method.addParameter(parameter);
+            }
+        }
     }
 }

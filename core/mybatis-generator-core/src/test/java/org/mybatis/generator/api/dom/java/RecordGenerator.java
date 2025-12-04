9c68f64c46fb0521785728c5e84653af74da418f
Jeff Butler <jeffgbutler@gmail.com>
2025-12-04 16:49:56 -0500

    Tests for record support in DOM

--- core/mybatis-generator-core/src/test/java/org/mybatis/generator/api/dom/java/RecordGenerator.java
+++ core/mybatis-generator-core/src/test/java/org/mybatis/generator/api/dom/java/RecordGenerator.java
@@ -1,6 +1,5 @@
 package org.mybatis.generator.api.dom.java;
 
-import java.io.Serializable;
 import java.util.List;
 
 /**
@@ -27,43 +26,43 @@ import java.util.List;
  */
 public class RecordGenerator {
 
-    public record Record<T>(int id, String description, T theT) implements Serializable {
-        private static final int f;
-
-        static {
-            f = 22;
-        }
-
-        public Record(int id, String description) {
-            this(id, description, null);
-        }
-
-        public static int ff() {
-            return f;
-        }
-
-        public int add(int a, int b) {
-            return a + b + f;
-        }
-
-        public String sayIt() {
-            return id + " " + description;
-        }
-
-        public static class Fred implements Flintstone {
-
-        }
-
-        public interface Flintstone {
-
-        }
-
-        public enum MyEnum {
-            A, B, C
-        }
-
-        public record InnerRecord(int id, String description) {}
-    }
+//    public record ComplexRecord<T>(int id, String description, T theT) implements Serializable {
+//        private static final int f;
+//
+//        static {
+//            f = 22;
+//        }
+//
+//        public ComplexRecord(int id, String description) {
+//            this(id, description, null);
+//        }
+//
+//        public static int ff() {
+//            return f;
+//        }
+//
+//        public int add(int a, int b) {
+//            return a + b + f;
+//        }
+//
+//        public String sayIt() {
+//            return id + " " + description;
+//        }
+//
+//        public static class Fred implements Flintstone {
+//
+//        }
+//
+//        public interface Flintstone {
+//
+//        }
+//
+//        public enum MyEnum {
+//            A, B, C
+//        }
+//
+//        public record InnerRecord(int id, String description) {}
+//    }
 
     public static List<CompilationUnit> generateTestClasses() {
         return List.of(simpleRecord(), complexRecord());
@@ -82,6 +81,7 @@ public class RecordGenerator {
         FullyQualifiedJavaType recordType = new FullyQualifiedJavaType("mbg.domtest.generators.records.ComplexRecord");
         TopLevelRecord topLevelRecord = new TopLevelRecord(recordType);
         topLevelRecord.addTypeParameter(new TypeParameter("T"));
+        topLevelRecord.addSuperInterface(new FullyQualifiedJavaType("java.io.Serializable"));
         topLevelRecord.setVisibility(JavaVisibility.PUBLIC);
         topLevelRecord.addParameter(new Parameter(new FullyQualifiedJavaType("int"), "id"));
         topLevelRecord.addParameter(new Parameter(new FullyQualifiedJavaType("String"), "description"));

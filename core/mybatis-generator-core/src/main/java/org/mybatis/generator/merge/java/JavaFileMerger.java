90b27dc0223283e5a5cccfd743bc8951235d5488
Jeff Butler <jeffgbutler@gmail.com>
2026-01-30 10:28:35 -0500

    Move the Java merger into its own package
    We will do this with the XML merger also

--- core/mybatis-generator-core/src/main/java/org/mybatis/generator/merge/java/JavaFileMerger.java
+++ core/mybatis-generator-core/src/main/java/org/mybatis/generator/merge/java/JavaFileMerger.java
@@ -13,7 +13,7 @@
  *    See the License for the specific language governing permissions and
  *    limitations under the License.
  */
-package org.mybatis.generator.internal;
+package org.mybatis.generator.merge.java;
 
 import static org.mybatis.generator.internal.util.messages.Messages.getString;
 
